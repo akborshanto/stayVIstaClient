a170d22f42e0684ea1de912be5225b7b1481d486
akbor shanto <akborshanto2002@gmail.com>
2024-05-26 03:42:14 -0700

    ream reservation price

--- src/components/RoomDetails/RoomReservation.jsx
+++ src/components/RoomDetails/RoomReservation.jsx
@@ -2,21 +2,26 @@ import PropTypes from 'prop-types'
 import Button from '../Shared/Button/Button'
 import { DateRange } from 'react-date-range';
 import { useState } from 'react';
+import { differenceInCalendarDays } from 'date-fns';
 const RoomReservation = ({ room }) => {
-
+console.log(room.from,room.to)
 
 
   const [state, setState] = useState([
     {
-      startDate: new Date(),
-      endDate: null,
+      startDate: new Date(room.from),
+      endDate: new Date(room.to),
       key: 'selection'
     }
   ]);
 
+/* date fns package */
+const totalDate=parseInt(
+  differenceInCalendarDays(new Date(room.to), new Date(room.from))
+  ) * room?.price
 
 
-
+console.log(totalDate)
   return (
     <div className='rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white'>
     
@@ -29,7 +34,12 @@ const RoomReservation = ({ room }) => {
       editableDateInputs={true}
       rangeColors={['#fb5200']}
       showDateDisplay={false}
-      onChange={item => setState([item.selection])}
+      onChange={item => setState([{
+
+        startDate: new Date(room.from),
+        endDate:new Date( room.to),
+        key:'selection'
+      }])}
       moveRangeOnFirstSelection={false}
       ranges={state}
     />}</div>
@@ -40,7 +50,7 @@ const RoomReservation = ({ room }) => {
       <hr />
       <div className='p-4 flex items-center justify-between font-semibold text-lg'>
         <div>Total</div>
-        <div>${room?.price}</div>
+        <div>{totalDate}</div>
       </div>
     </div>
   )
--- src/pages/Dashboard/host/RoomDataRow.jsx
+++ src/pages/Dashboard/host/RoomDataRow.jsx
@@ -44,11 +44,11 @@ const closeModal=()=>{
           <p className="text-gray-900 whitespace-no-wrap">${room?.price}</p>
         </td>
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p className="text-gray-900 whitespace-no-wrap">dsafd</p>
+          <p className="text-gray-900 whitespace-no-wrap">      {new Date(room?.from).toLocaleDateString()}</p>
         </td>
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
           <p className="text-gray-900 whitespace-no-wrap">
-            {(new Date(room?.to), "P")}
+            {new Date(room?.to).toLocaleDateString()}
           </p>
         </td>
         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
